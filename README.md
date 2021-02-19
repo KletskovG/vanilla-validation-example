f996c9f6d1eafc22fc7ed28c093afbf420f3d0fc
Gleb Kletksov <gfgfddgleb@gmail.com>
2021-02-19 19:54:17 +0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mail-ru
+https://stackoverflow.com/questions/39988675/form-validation-vanilla-js
+https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
--- main.js
+++ main.js
@@ -1,4 +1,22 @@
 window.onload = () => {
-  const form = document.querySelector("main__form");
-  
+  const form = document.querySelector(".main__form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("SUBMIT")
+    const form = document.forms["sign-up"];
+    const username = form["username"].value;
+    const email = form["email"].value;
+    const phone = form["phone"].value;
+    const image = form["avatar"].files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(image);
+    reader.onload = () => {
+      console.log(reader.result)
+    }
+    console.log(image)
+    console.log(form)
+    console.log(username)
+    console.log(email)
+    console.log(phone)
+  })  
 }
