241d39441569c12045ade5880e0723f87eda78ab
Gleb Kletksov <gfgfddgleb@gmail.com>
2021-02-22 09:26:11 +0300

    complete

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
+const PHONE_NUMBER_LENGTH = 11;
+const emailRegExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const USERNAME_MIN_LENGTH = 3;
+
 window.addEventListener("load", () => {
-  const form = document.forms["sign-up"]; // TODO: MAKE LOOKS SAME
+  const form = document.forms["sign-up"];
   
   form.addEventListener("submit", (event) => {
-    validateForm(event, form)
+    validateForm(event, form);
   });
   handleFileInput();
-})
+});
 
 function validateForm(event, form) {
-  event.preventDefault();
   let isFormValid = true;
 
   const fields = [
@@ -17,7 +20,7 @@ function validateForm(event, form) {
       value: form["username"].value,
       isValid() {
         if (this.value !== undefined && this.value !== null) {
-          return this.value.length > 3
+          return this.value.length > USERNAME_MIN_LENGTH;
         }
         return false;
       },
@@ -26,9 +29,9 @@ function validateForm(event, form) {
       name: "email",
       value: form["email"].value,
       isValid() {
-        const emailRegExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        
         if (this.value !== undefined && this.value !== null) {
-          return emailRegExp.test(this.value)
+          return emailRegExp.test(this.value);
         }
         return false;
       },
@@ -38,7 +41,7 @@ function validateForm(event, form) {
       value: form["phone"].value,
       isValid() {
         if (this.value !== undefined && this.value !== null) {
-          return /^\d+$/.test(this.value) && this.value.length === 11
+          return /^\d+$/.test(this.value) && this.value.length === PHONE_NUMBER_LENGTH;
         }
         return false;
       }
@@ -51,7 +54,7 @@ function validateForm(event, form) {
         return this.value !== undefined && this.value !== null;
       },
     }
-  ]
+  ];
 
   fields.forEach(field => {
     if (field.isValid() === false) {
@@ -64,10 +67,12 @@ function validateForm(event, form) {
   });
 
   if (isFormValid) {
+    // Hide errors
     [...document.querySelectorAll(".form__error")]
       .forEach(el => el.classList.toggle("hidden", true));
     
     successAlert();
+    event.preventDefault();
   } else {
     event.preventDefault();
     return;
@@ -76,23 +81,11 @@ function validateForm(event, form) {
 
 // Shows name of the file in avatar input
 function handleFileInput() {
-  const fileInput = document.querySelector("#avatar");
+  const fileInput = document.forms["sign-up"]["avatar"];
   fileInput.addEventListener("change", () => {
-    const fileNameElement = document.querySelector(".filename");
-    if (fileNameElement) {
-      fileNameElement.innerHTML = fileInput.files[0].name;
-    }
+    const fileNameElement = document.querySelector(".form__input_filename");
+    fileNameElement.innerHTML = fileInput.files[0].name;
   });
-}
-
-function successAlert() {
-  const submitButton = document.querySelector("button[type='submit']");
-  submitButton.classList.toggle("success", true);
-  submitButton.textContent = "Заполнено верно";
-
-  setTimeout(() => {
-    reset();
-  }, 2000)
 }
 
 function reset() {
@@ -109,6 +102,14 @@ function reset() {
   form["email"].value = "";
   form["avatar"].files[0] = null;
   form["avatar"].value = null;
-  document.querySelector(".filename").textContent = "";
+  document.querySelector(".form__input_filename").textContent = "";
   form["phone"].value = null;
+}
+
+function successAlert() {
+  const submitButton = document.querySelector("button[type='submit']");
+  submitButton.classList.toggle("success", true);
+  submitButton.textContent = "Заполнено верно";
+
+  setTimeout(reset, 1500);
 }
