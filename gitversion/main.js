3e77848735a0411d7c29fc842959e42b35f5b6be
Gleb Kletksov <gfgfddgleb@gmail.com>
2021-03-11 20:05:54 +0300

    add js validation

--- gitversion/main.js
+++ gitversion/main.js
@@ -1,14 +1,19 @@
 window.addEventListener("load" , () => {
-
+  const submitButton = document.querySelector(".submit");
+  submitButton.addEventListener("click", () => {
+    const form = document.forms["sign-up"];
+    validateForm(form);
+  });
 });
 
 function validateForm() {
   const form = document.forms["sign-up"];
-
+  // Check whole form and display native error messages
   if (form.checkValidity() === false) {
     form.reportValidity();
     return;
   }
   successAlert(form);
 }
+
 
